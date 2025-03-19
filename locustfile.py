f5e7cd831032ed30226ad8973c686a865b2c2bc6
Yerouch <101445240+Yerouch@users.noreply.github.com>
2025-03-19 23:44:50 +0000

    locust

--- locustfile.py
+++ locustfile.py
@@ -1,12 +1,12 @@
 from locust import HttpUser, task, between
 
 class WebsiteUser(HttpUser):
-    wait_time = between(1, 3) 
+    wait_time = between(1, 3)
 
     @task
     def load_main_page(self):
-        self.client.get("/")  
+        self.client.get("/")
 
     @task
-    def load_workorders(self):
-        self.client.get("/workorder/") 
+    def load_users(self):
+        self.client.get("/users/")
